36d5c20c0463e8b932cf610a43b6617684e01a17
Ninad Deshpande <52848314+Ninad09@users.noreply.github.com>
2020-04-16 08:36:14 +0530

    understanding tuples

---
+++ tuples.py
@@ -0,0 +1,13 @@
+#defining a tuple is done by using round brackets instead of square brackets (list) or curly brackets (dictionaries)
+words=("word0","word1","word2","word3")
+print(words)
+print(words[2])
+#words[3]="word4"    #reassignment is not allowed in tupes
+#tuples can also be created without parenthesis
+numbers=1,2,3,4,5
+print(numbers)
+squares="1","4","9","16","25"
+print(squares)
+#empty tuple
+tup=()
+print(tup)

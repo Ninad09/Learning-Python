d0061edc27f5a511c15cdadf9c4b10392fa3f74a
Ninad Deshpande <52848314+Ninad09@users.noreply.github.com>
2020-04-18 17:21:27 +0530

    Add files via upload

---
+++ fileops.py
@@ -0,0 +1,10 @@
+file=open("ninad.txt","r+")
+#for i in file:
+    #print(i)
+#for i in range(5):
+#    contents=file.read(4)
+#    print(contents)
+#print(file.read())
+#print(len(file.read()))
+print(file.readlines())
+file.close()

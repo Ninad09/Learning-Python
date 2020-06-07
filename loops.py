79044390c97fb0c9a9ff6ac1876fe72ee2aa6ab3
Ninad Deshpande <52848314+Ninad09@users.noreply.github.com>
2020-06-07 17:19:43 +0530

    Simple 'for' and 'while' loop

--- loops.py
+++ loops.py
@@ -3,3 +3,7 @@ for word in words:
     print(word+'!')
 for i in range(7):
     print("hello!")
+
+while words:
+    print(words)
+    #prints the list again and again until the repititions match the number of elements in the list
